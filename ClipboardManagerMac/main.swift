b1d581b92ffd4d3dfe3faf27424caa06711c9aaf
siddhantkushwaha <k16.siddhant@gmail.com>
2021-07-05 13:06:52 +0530

    Socket programming on this is going to be really hard.

--- ClipboardManagerMac/main.swift
+++ ClipboardManagerMac/main.swift
@@ -1,11 +1,63 @@
-//
-//  main.swift
-//  ClipboardManagerMac
-//
-//  Created by Siddhant Kushwaha on 04/07/21.
-//
+import Cocoa
 
-import Foundation
+let pasteboard: NSPasteboard = NSPasteboard.general
+var count: Int = pasteboard.changeCount
 
-print("Hello, World!")
-
+repeat{
+    
+    usleep(5 * 1000 * 1000)
+    
+    if(count < pasteboard.changeCount) {
+        count = pasteboard.changeCount
+        
+        var found = false
+        for type in pasteboard.types ?? [] {
+            switch type {
+            
+            // files will be handled later on in project Unity
+            case .fileURL:
+                print("Files were copied.")
+                let fileURls = pasteboard.readObjects(forClasses: [NSURL.self], options: nil) ?? []
+                if (fileURls.isEmpty) {
+                    continue
+                }
+                
+                for fileURL in fileURls {
+                    print(fileURL)
+                }
+                
+                // in current pasteboard if file urls are present,
+                // same filenames are also present as string which have to be discarded
+                found = true
+                
+                break
+                
+            // string should have the lowest priority
+            case .string:
+                print("New text added to clipboard.")
+                let stringData = pasteboard.string(forType: type) ?? ""
+                if (stringData.isEmpty) {
+                    continue
+                }
+                
+                print(stringData)
+                
+                // string has lowest priority
+                found = true
+                
+                break
+            default:
+                
+                print("Data format not supported as of now.")
+                
+                break
+            }
+            
+            if (found) {
+                break
+            }
+        }
+        
+    }
+    
+} while true
